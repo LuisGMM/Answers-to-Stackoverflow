c00b9b647e61a9942cef10d1f29849844ecec4bf
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-19 22:08:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,13 @@ dict1 = {'key1a': [i for i in range(5)], 'key1b': [i for i in range(6)]}
 dict2 = {'key2a': [i for i in range(5)], 'key2b': [i for i in range(6)]}
 
 
+def elements_in_common(list1: list, list2: list) -> float:
+
+    biggest_list = list1 if len(list1) >= len(list2) else list2
+    smallest_list = list1 if biggest_list is list1 else list2
+    
+    elements_in_common = [element for element in biggest_list if element in smallest_list]
+
+    return elements_in_common
+
 
